a11e14946a42215027979c732959451cb95c9181
Dipayan Pramanik <dipayan.pramanik@knoldus.com>
2023-04-03 14:02:46 +0530

    Added lamda function

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -10,9 +10,9 @@ terraform {
     storage_account_name = "adostoragetest"
     container_name       = "tfbackend"
     key                  = "resourcegroup/terraform.tfstate"
-    resource_group_name  = "ado-rg"
-    use_msi              = true
-    subscription_id      = "b00e9e00-4924-464f-929d-7c28ccf1a449"
-    tenant_id            = "19808729-db27-4d6e-8315-1ba7d75db983"
+    # resource_group_name  = "ado-rg"
+    # use_msi              = true
+    # subscription_id      = "b00e9e00-4924-464f-929d-7c28ccf1a449"
+    # tenant_id            = "19808729-db27-4d6e-8315-1ba7d75db983"
   }
 }
